247c8f1898f975c9f7f09a55ee65985cfb40426c
antoniogit95 <joseantoniovargastellez@gmail.com>
2023-10-17 09:29:58 -0400

    agregar la funcionalidad de validar cuenta

--- api/src/main/java/com/example/api/User/UserController.java
+++ api/src/main/java/com/example/api/User/UserController.java
@@ -1,33 +1,64 @@
 package com.example.api.User;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.RequiredArgsConstructor;
+import com.example.api.Person.Person;
+import com.example.api.Person.PersonService;
+
+import lombok.AllArgsConstructor;
 
 @RestController
 @RequestMapping("/api/v1/user")
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class UserController {
     
     UserRepository userRepository;
+    PersonService personService;
 
-    /**@PutMapping("/active/{idPersona}")
+    @PutMapping("/active/{id_person}")
     public ResponseEntity<String> activarUsuarioPorIdPersona(@PathVariable Long id_person) {
-    System.out.println("-----------------------------------------\n"+
-    "logrando entrar a obtener datos del usuaro a travez de la persona\n"+
-    "-------------------------------------------------");
-    
-    User user = userRepository.findByPersonIdPerson(id_person);
-    if (user != null) {
-        user.setStatus(true);
-        userRepository.save(user);
-        return ResponseEntity.ok("Usuario activado exitosamente");
-    } else {
-        return ResponseEntity.notFound().build();
+        Person person = personService.getPersonById(id_person);
+        Optional<User> userOptional = userRepository.findByPerson(person);
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            user.setStatus(true);
+            user.setUpdate_at(getTimestamp());
+            userRepository.save(user);
+            return ResponseEntity.ok("Usuario activado exitosamente");
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
-}*/
+
+    @GetMapping("/isValidate/{id_person}")
+    public ResponseEntity<Boolean> isValidateUser(@PathVariable Long id_person) {
+        System.out.println("\n---------------------------------------------------------------\n"
+        +"entrando al servidor"+"\n---------------------------------------------------------------\n");
+        Person person = personService.getPersonById(id_person);
+        Optional<User> userOptional = userRepository.findByPerson(person);
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            if(user.isStatus()){
+                return ResponseEntity.ok(true);
+            }else{
+                return ResponseEntity.ok(false);
+            }
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    private Timestamp getTimestamp(){
+        LocalDateTime now = LocalDateTime.now();
+        return Timestamp.valueOf(now);
+    }
 }
--- api/src/main/java/com/example/api/User/UserRepository.java
+++ api/src/main/java/com/example/api/User/UserRepository.java
@@ -5,11 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import com.example.api.Person.Person;
+
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     @EntityGraph(attributePaths = "person")
     Optional<User> findByUsername(String username);
-    //User findByPersonIdPerson(Long idPerson);
+    Optional<User> findByPerson(Person person);
 }
