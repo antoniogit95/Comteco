894f94af0bf1aeb6c26642be975261c8a4a5b4e5
antoniogit95 <joseantoniovargastellez@gmail.com>
2023-11-22 08:16:24 -0400

    agregar llaves foraneas a la tabla orden dia

--- api/src/main/java/com/example/api/ordenDia/OrdenDia.java
+++ api/src/main/java/com/example/api/ordenDia/OrdenDia.java
@@ -2,9 +2,18 @@ package com.example.api.ordenDia;
 
 import java.sql.Timestamp;
 
+import com.example.api.Routes.Pos.Pos;
+import com.example.api.ordenDia.servicio.Servicio;
+import com.example.api.ordenDia.solicitud.Solicitud;
+import com.example.api.ordenDia.trabajo.Trabajo;
+import com.example.api.ordenDia.ubicacionTecnica.UbicacionTecnica;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -22,13 +31,30 @@ public class OrdenDia {
     private Long id;
 
     private Timestamp fecha;
-    private Long idSolicitud;
-    private Long idTrabajo;
-    private Long idServicio;
-    private Long idUbicacionTecnica;
+
+    @ManyToOne
+    @JoinColumn(name = "id_solicitud")
+    private Solicitud solicitud;
+
+    @ManyToOne
+    @JoinColumn(name = "id_trabajo")
+    private Trabajo trabajo;
+
+    @ManyToOne
+    @JoinColumn(name = "id_servicio")
+    private Servicio servicio;
+
+    @ManyToOne
+    @JoinColumn(name = "id_ubicacion_tecnica")
+    private UbicacionTecnica ubicacionTecnica;
+
+    @Column(unique = true)
     private Long producto;
     private boolean estado;
-    private Long idPos;
+
+    @ManyToOne
+    @JoinColumn(name = "id_pos")
+    private Pos pos;
     private Long idCliente;
     private boolean estadoOt;
     private Long idTecnico;
