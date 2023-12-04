f21398571f03812f598d67a83050567820979351
jpvm9537 <jpvm953718@gmail.com>
2023-12-04 06:10:27 -0800

    se quito el footer y la hora del sistemas e muestra en el navbar, tambiens e agrego un logo de comteco

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {  RouterProvider } from 'react-router-dom';
 import { Rutas } from './rutas/Rutas';
 import { AuthProvider } from './providerContext/AuthProvider';
+import { Footer } from "../src/componetes/Footer/Footer";
 
 function App() {
 
   return (
       <AuthProvider>
         <RouterProvider router={Rutas} />
+        
       </AuthProvider>
   );
 }
