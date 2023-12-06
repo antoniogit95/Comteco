14ba8d7c298af4dff9f751059c59fbaedfb7aa10
antoniogit95 <joseantoniovargastellez@gmail.com>
2023-12-06 11:22:26 -0400

    implementar el metodo para resivir el archivo desde el cliente con los parametros 'file'

--- backend/src/main/java/comteco/backend/ordenDia/ORdenDiaController.java
+++ backend/src/main/java/comteco/backend/ordenDia/ORdenDiaController.java
@@ -2,6 +2,7 @@ package comteco.backend.ordenDia;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +11,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import lombok.AllArgsConstructor;
 
@@ -18,31 +21,46 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 @RequestMapping("/api/v1/orden_dia")
 public class ORdenDiaController {
+    
     private  OrdenDiaService ordenDiaService;
 
+    @PostMapping("/save_file")
+    public ResponseEntity<String> saveOrdenDiaFile(@RequestParam("file") MultipartFile file) {
+        if(file != null){
+            return ordenDiaService.saveFile(file);
+        }else{
+            return new ResponseEntity<>("el archivo esta vacio", HttpStatus.ACCEPTED);
+        }
+    }
+    
+
     @GetMapping
-    public ResponseEntity<List<OrdenDiaResponse>> getAllOrdenDias() {
-        return ordenDiaService.getAllOrdenDias();
+    public ResponseEntity<List<OrdenDia>> getAllOrdenDias() {
+        List<OrdenDia> ordenDias = ordenDiaService.getAllOrdenDias();
+        return new ResponseEntity<>(ordenDias, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<OrdenDiaResponse> getOrdenDiaById(@PathVariable Long id) {
-        return ordenDiaService.getOrdenDiaById(id);
+    public ResponseEntity<OrdenDia> getOrdenDiaById(@PathVariable Long id) {
+        return ordenDiaService.getOrdenDiaById(id)
+                .map(ordenDia -> new ResponseEntity<>(ordenDia, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @PostMapping
-    public ResponseEntity<OrdenDia> createOrdenDia(@RequestBody OrdenDia ordenDia) {
-        return ordenDiaService.createOrdenDia(ordenDia);
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<OrdenDia> updateOrdenDia(@PathVariable Long id, @RequestBody OrdenDia ordenDia) {
-        return ordenDiaService.updateOrdenDia(id, ordenDia);
+    public ResponseEntity<OrdenDia> saveOrdenDia(@RequestBody OrdenDia ordenDia) {
+        OrdenDia savedOrdenDia = ordenDiaService.saveOrdenDia(ordenDia);
+        return new ResponseEntity<>(savedOrdenDia, HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Boolean> deleteOrdenDia(@PathVariable Long id) {
-        return ordenDiaService.deleteOrdenDia(id);
-        
+    public ResponseEntity<Void> deleteOrdenDia(@PathVariable Long id) {
+        ordenDiaService.deleteOrdenDia(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<OrdenDia> editOrdenDia(@PathVariable String id, @RequestBody OrdenDia ordenDia) { 
+        return new ResponseEntity<>(ordenDia, HttpStatus.OK);
     }
 }
--- backend/src/main/java/comteco/backend/ordenDia/OrdenDiaService.java
+++ backend/src/main/java/comteco/backend/ordenDia/OrdenDiaService.java
@@ -1,10 +1,13 @@
 package comteco.backend.ordenDia;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.List;
+import java.util.Optional;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import lombok.AllArgsConstructor;
 
@@ -12,34 +15,41 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class OrdenDiaService {
 
-    //private OrdenDiaRepository ordenDiaRepository;
+    private OrdenDiaRepository ordenDiaRepository;
 
-    public ResponseEntity<List<OrdenDiaResponse>> getAllOrdenDias() {
+    public List<OrdenDia> getAllOrdenDias() {
+        return ordenDiaRepository.findAll();
+    }
+
+    public Optional<OrdenDia> getOrdenDiaById(Long id) {
+        return ordenDiaRepository.findById(id);
+    }
+
+    public OrdenDia saveOrdenDia(OrdenDia ordenDia) {
+        return ordenDiaRepository.save(ordenDia);
+    }
+
+    public void deleteOrdenDia(Long id) {
+        ordenDiaRepository.deleteById(id);
+    }
+
+    public ResponseEntity<String> saveFile(MultipartFile file) {
         try {
-            //List<OrdenDia> ordenDia = ordenDiaRepository.findAll();
-            return new ResponseEntity<>(null, HttpStatus.OK);
+            BufferedReader br = new BufferedReader(new InputStreamReader(file.getInputStream()));
+            String linea;
+            boolean firstLine = true;
+            while ((linea = br.readLine()) != null) {
+                if(firstLine){
+                    firstLine = false;
+                    continue;
+                }
+                String partes[] = linea.split(";");
+                System.out.println(partes.length+"---------------------"+partes[0]+"---------------");
+            }
         } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+            // TODO: handle exception
         }
-    }
 
-    public ResponseEntity<OrdenDiaResponse> getOrdenDiaById(Long id) {
         return null;
     }
-
-    public ResponseEntity<OrdenDia> createOrdenDia(OrdenDia ordenDia) {
-        return null;
-    }
-
-    public ResponseEntity<OrdenDia> updateOrdenDia(Long id, OrdenDia ordenDia) {
-        return null;
-    }
-
-    public void createOrdenDia(Long id) {
-    }
-
-    public ResponseEntity<Boolean> deleteOrdenDia(Long id) {
-        return null;
-    }
-    
 }
