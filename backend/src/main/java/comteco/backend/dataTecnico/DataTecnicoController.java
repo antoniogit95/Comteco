7bc3d69520b9a7b04b690d3b4193e099e6ddd223
antoniogit95 <joseantoniovargastellez@gmail.com>
2023-12-20 09:14:01 -0400

    crear los registros de DatoTecnico

---
+++ backend/src/main/java/comteco/backend/dataTecnico/DataTecnicoController.java
@@ -0,0 +1,38 @@
+package comteco.backend.dataTecnico;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/v1/data_tecnico")
+public class DataTecnicoController {
+    
+    private DataTecnicoService dataTecnicoService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DataTecnico> getDataTecnicoById(@PathVariable Long id) {
+        return dataTecnicoService.getDataTecnicoById(id)
+            .map(servicio -> new ResponseEntity<>(servicio, HttpStatus.OK))
+            .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping()
+    public ResponseEntity<DataTecnico> saveDataTecnico(@RequestBody DataTecnico dataTecnico) {
+        DataTecnico savedDataTecnico = dataTecnicoService.saveDataTecnico(dataTecnico);
+        return new ResponseEntity<>(savedDataTecnico, HttpStatus.CREATED);
+    }
+    
+    
+}
---
+++ backend/src/main/java/comteco/backend/dataTecnico/DataTecnicoService.java
@@ -0,0 +1,16 @@
+package comteco.backend.dataTecnico;
+
+import java.util.Optional;
+
+public class DataTecnicoService {
+
+    private DataTecnicoRepository dataTecnicoRepository;
+    public Optional<DataTecnico> getDataTecnicoById(Long id) {
+        return dataTecnicoRepository.findById(id);
+    }
+
+    public DataTecnico saveDataTecnico(DataTecnico dataTecnico) {
+        return dataTecnicoRepository.save(dataTecnico);
+    }
+    
+}
