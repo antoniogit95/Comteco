eb3f1f39aed3cbdbbd56fccbfdc294b08e42929e
antoniogit95 <joseantoniovargastellez@gmail.com>
2023-12-27 11:01:27 -0400

    establecer 10 minutos para que refresque el toquen y 30 minutos de inactividad se cerrara la sesion

--- frontend/src/providerContext/AuthProvider.jsx
+++ frontend/src/providerContext/AuthProvider.jsx
@@ -1,4 +1,6 @@
 import React, {useContext, useState, createContext, useEffect} from "react";
+import axios from "axios";
+import { URL_API_private } from "./EndPoint";
 
 const AuthContext  = createContext({
     isAuthenticated: false,
@@ -15,33 +17,22 @@ export const AuthProvider = ({ children }) =>{
     });
     const [accessToken, setAccessToekn] = useState("");
     const [timeOut, setTimeOut] = useState("");
+    const endPoint = URL_API_private+"/refresh_token"
+
+    const INACTIVITY_TIMEOUT = 31 * 60 * 1000; // 31 minutos en milisegundos
+    const REFRESH_INTERVAL = 10*60*1000 //20 en milisegundos
 
     function getAccessToken(){
         return accessToken;
     }
 
-    useEffect(() => {
-        const checkTokenExpiration = () => {
-            if (timeOut && new Date() > timeOut) {
-                handleLogout();
-            }
-        };
-    
-        checkTokenExpiration();
-        const intervalId = setInterval(checkTokenExpiration, 5000); // Verificar cada segundo
-    
-        return () => {
-            clearInterval(intervalId);
-        };
-
-    }, [timeOut]);
 
     const handleLogout = () => {
         setAccessToekn("");
         localStorage.removeItem('user_data');
         setIsAuthenticated(false);
     };
-
+    
     function deletToken(){
         setAccessToekn("");
         localStorage.removeItem('user_data');
@@ -69,6 +60,53 @@ export const AuthProvider = ({ children }) =>{
         setIsAuthenticated(true);
         setTimeOut(new Date(decodedToken.exp * 1000));
     }
+
+
+    useEffect(() => {
+        const checkTokenExpiration = () => {
+            if (timeOut && new Date() > timeOut) {
+                handleLogout();
+            }
+        };
+    
+        checkTokenExpiration();
+        const intervalId = setInterval(checkTokenExpiration, 5000); // Verificar cada segundo
+    
+        return () => {
+            clearInterval(intervalId);
+        };
+
+    }, [timeOut]);
+
+    useEffect(() => {
+        const handleUserActivity = async () => {
+            if(getAccessToken() && now - lastRefreshTime.current >= REFRESH_INTERVAL){
+                try {
+                    const username  = JSON.parse(localStorage.getItem('user_data')).username;
+                    const response = await axios.post(endPoint, {username}, {headers: {
+                        'Authorization': `Bearer ${getAccessToken()}`,
+                    }},);
+                    saveToken(response.data)
+                } catch (error) {
+                    console.error('Error al obtener un nuevo token:', error);   
+                }
+                lastRefreshTime.current = now;
+            }
+        };
+    
+        const logoutOnInactivity = setTimeout(() => {
+            handleLogout();
+        }, INACTIVITY_TIMEOUT);
+    
+        document.addEventListener('mousemove', handleUserActivity);
+        document.addEventListener('keypress', handleUserActivity);
+    
+        return () => {
+            clearTimeout(logoutOnInactivity);
+            document.removeEventListener('mousemove', handleUserActivity);
+            document.removeEventListener('keypress', handleUserActivity);
+        };
+    }, [getAccessToken]);
 
     return <AuthContext.Provider value={{ isAuthenticated, getAccessToken, saveToken, deletToken }}>
         {children}
