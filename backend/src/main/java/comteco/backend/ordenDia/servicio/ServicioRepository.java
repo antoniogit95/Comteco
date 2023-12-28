03852b734eb9e1f5a4006ded13d6430f8d01b744
antoniogit95 <joseantoniovargastellez@gmail.com>
2023-12-28 11:59:20 -0400

    lograr desplegar la 3 fila en los reportes con los datos mensionados

--- backend/src/main/java/comteco/backend/ordenDia/servicio/ServicioRepository.java
+++ backend/src/main/java/comteco/backend/ordenDia/servicio/ServicioRepository.java
@@ -1,9 +1,15 @@
 package comteco.backend.ordenDia.servicio;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import comteco.backend.ordenDia.OrdenDia;
+
 @Repository
 public interface ServicioRepository extends JpaRepository<Servicio, Long>{
+
+    List<Servicio> findAllByOrdenDia(OrdenDia ordenDia);
     
 }
