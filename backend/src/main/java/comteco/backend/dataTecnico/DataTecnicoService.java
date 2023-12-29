394bdb5d14d51d9644e9286ff16fc80908dbedb4
antoniogit95 <joseantoniovargastellez@gmail.com>
2023-12-29 09:14:13 -0400

    arreglar un ;

--- backend/src/main/java/comteco/backend/dataTecnico/DataTecnicoService.java
+++ backend/src/main/java/comteco/backend/dataTecnico/DataTecnicoService.java
@@ -254,6 +254,7 @@ public class DataTecnicoService {
                     .build();
                     responses.add(dtrr);
                 }
+            }
         }
         return responses;
     }
