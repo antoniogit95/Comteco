ea6c5944cd206f88bf41b47eae367c12a09b439e
antoniogit95 <joseantoniovargastellez@gmail.com>
2024-01-04 10:53:31 -0400

    comentar las clases de Serivicio y su controlador

--- backend/src/main/java/comteco/backend/ordenDia/servicio/ServicioController.java
+++ backend/src/main/java/comteco/backend/ordenDia/servicio/ServicioController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
 
+/**
+ * Controlador de la clase servicio
+ */
 @RestController
 @RequestMapping("/api/v1/servicio")
 @AllArgsConstructor
@@ -21,6 +23,11 @@ public class ServicioController {
     
     private ServicioService servicioService;
 
+    /**
+     * 
+     * @param producto a ser buscado en los servicios
+     * @return todos los servicios relacionados al producto
+     */
     @GetMapping("/producto/{producto}")
     public ResponseEntity<List<ServicioResponse>> getAllServiciosByProduct(@PathVariable Long producto){
         System.out.println("Producto recibido: " + producto);
@@ -28,12 +35,20 @@ public class ServicioController {
         return new ResponseEntity<>(servicios, HttpStatus.OK);
     }
 
+    /**
+     * 
+     * @return todos los servicios registrados
+     */
     @GetMapping
     public ResponseEntity<List<ServicioResponse>> getAllServicios() {
         List<ServicioResponse> servicios = servicioService.getAllServicios();
         return new ResponseEntity<>(servicios, HttpStatus.OK);
     }
 
+    /**
+     * @param id del servicio a ser buscado
+     * @return el servicio si es que existe caso contratio retorna notfound.
+     */
     @GetMapping("/{id}")
     public ResponseEntity<Servicio> getServicioById(@PathVariable Long id) {
         return servicioService.getServicioById(id)
@@ -41,15 +56,14 @@ public class ServicioController {
                 .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
+    /**
+     * 
+     * @param servicio Registra un servicio 
+     * @return le mismo servicio pero ya creado
+     */
     @PostMapping
     public ResponseEntity<Servicio> saveServicio(@RequestBody Servicio servicio) {
         Servicio savedServicio = servicioService.saveServicio(servicio);
         return new ResponseEntity<>(savedServicio, HttpStatus.CREATED);
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteServicio(@PathVariable Long id) {
-        servicioService.deleteServicio(id);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
