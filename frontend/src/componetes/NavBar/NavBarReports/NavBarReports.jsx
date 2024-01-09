08fd9170eb816eb1b100ae030d280ea0c02bd389
antoniogit95 <joseantoniovargastellez@gmail.com>
2024-01-09 10:44:24 -0400

    cambiar en el navBar de los reportes de Reportes a Producto

--- frontend/src/componetes/NavBar/NavBarReports/NavBarReports.jsx
+++ frontend/src/componetes/NavBar/NavBarReports/NavBarReports.jsx
@@ -3,8 +3,8 @@ import './NavBarReports.css'
 export const NavBarReports = ({showReport}) => {
     return (<div className='stylesNavBarReport'>
 
-        <button className='stylesButoonNavBar' onClick={() => showReport("jose")}>Reportes</button>    
+        <button className='stylesButoonNavBar' onClick={() => showReport("jose")}>Producto</button>    
         <button className='stylesButoonNavBar' onClick={() => showReport("david")}>Analista</button>
-        <button className='stylesButoonNavBar' onClick={() => showReport("map")}> Map de Odf</button>
+        <button className='stylesButoonNavBar' onClick={() => showReport("map")}> Rutas Naps</button>
     </div>);
 }
