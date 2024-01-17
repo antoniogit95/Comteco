056a75dc8a46c0a9e0f4f2309153d5edc8d670a4
antoniogit95 <joseantoniovargastellez@gmail.com>
2024-01-17 09:30:08 -0400

    bloquear la parte del input para no ingresar datos, los datos que ya fueron llenados

--- frontend/src/componetes/Tables/TablesArchivo/DatosProducto.jsx
+++ frontend/src/componetes/Tables/TablesArchivo/DatosProducto.jsx
@@ -20,7 +20,7 @@ const ExcelTable = ({ producto }) => {
   const endPointDtaProduct = URL_API_private+"/orden_dia/producto/"+producto
   const endPointDataTecnico = URL_API_private +"/data_tecnico"
   const [dataProduct, setDataProdcut] = useState([]);
-  const [isLLenado, setIsLlenado] = useState(true);
+  const [isLLenado, setIsLlenado] = useState(false);
 
   useEffect(() => {
     getAlLCods();
@@ -38,6 +38,7 @@ const ExcelTable = ({ producto }) => {
       console.log(endPointDtaProduct+" "+config.headers)
       const response = await axios.get(endPointDtaProduct, config);
       setDataProdcut(response.data);
+      setIsLlenado(response.data[0].status)
       console.log(dataProduct);
       console.log("Products Recuperados exitosamente")
   } catch (error) {
@@ -75,7 +76,7 @@ const ExcelTable = ({ producto }) => {
       console.log("Respuesta del Servidor: ", response.data);
       console.log("Registro Tecnico Registrado exitosamente.");
       toast.success("Registro Tecnico Registrado exitosamente");
-      setIsLlenado(false);
+      setIsLlenado(true);
     }catch (error) {
       if(error.response.data){
         console.error("Error del Servidor: "+ error.response.data.message);
@@ -130,7 +131,7 @@ const ExcelTable = ({ producto }) => {
             <td className='stylesTh-Td'>{dataProduct[0].nap}</td>
             <td className='stylesTh-Td' >{dataProduct[0].posicion}</td>
             <td className='stylesTh-Td' >
-              {isLLenado? (<>
+              {isLLenado? "Ya fue llenado": (<>
                 <input
               type="text"
               value={texto}
@@ -148,7 +149,7 @@ const ExcelTable = ({ producto }) => {
                   ))}
                 </datalist>
                 <button className='stylesButoon' onClick={() => guardarDato(event)}>Guardar</button>
-              </>):"ya fue llenado"}
+              </>)}
             
             </td>
             <td className='stylesTh-Td' >{dataProduct[0].datoTecnico}</td>
