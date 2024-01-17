4afe395d2473d8ee146512e10cfdb5a506429202
antoniogit95 <joseantoniovargastellez@gmail.com>
2024-01-17 09:15:04 -0400

    agregar la forma de debolver la busqueda de orden dis por producto, ahora devolvera un stado

--- backend/src/main/java/comteco/backend/ordenDia/OrdenDiaService.java
+++ backend/src/main/java/comteco/backend/ordenDia/OrdenDiaService.java
@@ -283,6 +283,7 @@ public class OrdenDiaService {
                 .datoTecnico(ordenDia.getPosicion().getNap().getCod()+"-"+ordenDia.getPosicion().getCod())
                 .zona(ordenDia.getUbicacion())
                 .ubicacion("sin ubicacion geografica")
+                .status(ordenDia.isEstadoOt())
                 //.direccion(ordenDia.getDireccion())
                 .build();
             response.add(oByProducto);
@@ -320,8 +321,24 @@ public class OrdenDiaService {
         
     }
 
-    public void validateDatoTecnico(Long producto) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'validateDatoTecnico'");
+    /**
+     * Validar los datos tecnicos por medio del producto.
+     * @param producto a ser validado y no poder cambiar el dato tecnico
+     */
+    public Boolean validateDatoTecnico(Long producto) {
+        try {
+            Optional<OrdenDia> ordenDiaOptional = ordenDiaRepository.findByProducto(producto);
+            if(ordenDiaOptional.isPresent()){
+                OrdenDia ordenDia = ordenDiaOptional.get();
+                ordenDia.setEstadoOt(true);
+                ordenDiaRepository.save(ordenDia);
+                return true;
+            }else{
+                return false;
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+            return false;
+        }
     }
 }
